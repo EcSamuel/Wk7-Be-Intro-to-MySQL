4f9d56bd00d69d8cd49edfe3a0745ccea277246f
Ethan Samuel <samuelethanc@gmail.com>
2024-07-22 11:53:40 -0500

    All Pieces Present for Submission

---
+++ mysql-java/src/main/java/projects/dao/DbConnection.java
@@ -0,0 +1,33 @@
+package projects.dao;
+
+import projects.exception.DbException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    private static String HOST = "localhost";
+    private static String PASSWORD = "projects";
+    private static int PORT = 3306;
+    private static String SCHEMA = "projects";
+    private static String USER = "projects";
+
+    public static Connection getConnection() {
+        // a.     Create a String variable named uri that contains the MySQL connection URI.
+        String uri = "jdbc:mysql://" + HOST + ":" + PORT + "/" + SCHEMA;
+
+        try {
+            // b.     Call DriverManager to obtain a connection. Pass the connection string (URI) to DriverManager.getConnection().
+            Connection connection = DriverManager.getConnection(uri, "yourusername", "yourpassword");
+            // d.     Print a message to the console (System.out.println) if the connection is successful.
+            System.out.println("Connected to database");
+        // c.      Surround the call to DriverManager.getConnection() with a try/catch block. The catch block should catch SQLException.
+        } catch (SQLException e) {
+            // I wasn't sure if the manual message would work so I printed it twice plus the throw
+            System.err.println(e.getMessage());
+            // e.     Print an error message to the console if the connection fails. Throw a DbException if the connection fails.
+            System.err.println("Manual Message: Error Connecting to the Database");
+            throw new DbException("Unable to Connect to the Database", e);
+        }
+    }
+}
